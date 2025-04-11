61c77d23570abbf8e445e26fbb2380836cce6b87
LuisMoreno28 <luis.2895@hotmail.com>
2025-04-10 20:36:08 -0600

    evaluacion1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,45 @@
 import { Component } from '@angular/core';
+import { TaskListComponent } from './task-list/task-list.component';
 
 @Component({
   selector: 'app-root',
+  imports: [TaskListComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-testing-project';
+  nuevatarea: string = '';
+  tareas: string[] = [
+    'Comprar',
+    'Limpiar'
+  ];
+
+
 
   addTask(task: string) {
+    this.tareas.push(task);
+
 
   }
 
+
   deleteTask(task: string) {
+    const index = this.tareas.indexOf(task);
+    if (index > -1) {
+      this.tareas.splice(index, 1);
+    }
+    
   }
 
   getTasks(): string[] {
     return [];
+  }
+
+  updateTask(task: string) {
+    const index = this.tareas.indexOf(task);
+    if (index > -1) {
+      this.tareas[index] = task;
+    }
   }
 }
